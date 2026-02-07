812a4db97bcd41e0c7d41202005e0652c141ce11
Adasi Kaakyire <Skyhuppa@gmail.com>
2026-02-07 15:01:08 +0000

    Update main.go

--- cmd/release_config/build_flag/main.go
+++ cmd/release_config/build_flag/main.go
@@ -215,7 +215,7 @@ func GetCommand(configs *rc_lib.ReleaseConfigs, commonFlags Flags, cmd string, a
 				// trunk_staging       ""
 				// trunk               ""
 				//
-				// ap1a                ""
+				// bp1a                ""
 				// ...
 				switch {
 				case config.Name == "trunk_staging":
