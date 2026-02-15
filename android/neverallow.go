71573a94a9607e2de36b3b26ac0436a06d5b7c74
Adasi Kaakyire <Skyhuppa@gmail.com>
2026-02-15 15:09:00 +0000

    Update neverallow.go

--- android/neverallow.go
+++ android/neverallow.go
@@ -250,7 +250,7 @@ func createInstallInRootAllowingRules() []Rule {
 			NotModuleType("prebuilt_system").
 			NotModuleType("prebuilt_first_stage_ramdisk").
 			NotModuleType("prebuilt_res").
-			NotModuleType("prebuilt_any").
+		//	NotModuleType("prebuilt_any").
 			Because("install_in_root is only for init_first_stage or librecovery_ui_ext."),
 	}
 }
